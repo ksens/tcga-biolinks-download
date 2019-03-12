5caf74c22caa8a64cf0a356867e249e75396d065
Kriti Sen Sharma <ksen@paradigm4.com>
2019-03-12 21:31:03 +0530

    add workflow type loop in gene expression download

--- download_genexp.R
+++ download_genexp.R
@@ -4,34 +4,46 @@ library(TCGAbiolinks)
 source('~/coding/downloads/tcga-biolinks-download/functions.R')
 
 tcga_subprojects = formulate_tcga_subproject_dataframe()
-
-idx = 1
-for (idx in 1:length(tcga_subprojects$name)) {
-  proj = tcga_subprojects$name[idx]
-  
-  
+workflow_types = c(
+  "HTSeq - FPKM-UQ",
+  "HTSeq - FPKM",
+  "HTSeq - Counts"
+)
+for (workflow_type in workflow_types) {
   message("==============================================")
-  message("idx: ", idx, " of ", nrow(tcga_subprojects), " --- TCGA subproject: ", proj)
+  message("workflow type: ", workflow_type)
   message("==============================================")
   
-  genexp_filename = paste0('~/Downloads/', proj, '-clinical_data.Rda')
-  
-  if (!file.exists(genexp_filename)) {
-    # load(paste0('~/Downloads/', proj, '-clinical_data.Rda'))
+  for (idx in 1:length(tcga_subprojects$name)) {
+    proj = tcga_subprojects$name[idx]
+    message("==============================================")
+    message("idx: ", idx, " of ", nrow(tcga_subprojects), " --- TCGA subproject: ", proj)
+    message("==============================================")
     
-    query <- GDCquery(project = proj,
-                      data.category = "Transcriptome Profiling",
-                      data.type = "Gene Expression Quantification",
-                      workflow.type = "HTSeq - FPKM-UQ")
-    GDCdownload(query)
-    data <- GDCprepare(query)
+    genexp_filename = paste0(
+      '~/Downloads/', 
+      proj, '--', 
+      gsub(" ", "", workflow_type), 
+      '--genexp_data_grch38.Rda')
     
-    # df1 = as.data.frame(colData(data))
-    # matrix_data = assay(data)
-    save(list = 'data',
-         file = genexp_filename
-    )
-  } else {
-    cat("Skipping since already downloaded\n")
+    if (!file.exists(genexp_filename)) {
+      # load(paste0('~/Downloads/', proj, '-clinical_data.Rda'))
+      
+      query <- GDCquery(project = proj,
+                        data.category = "Transcriptome Profiling",
+                        data.type = "Gene Expression Quantification",
+                        workflow.type = workflow_type)
+      GDCdownload(query)
+      data <- GDCprepare(query)
+      
+      # df1 = as.data.frame(colData(data))
+      # matrix_data = assay(data)
+      save(list = 'data',
+           file = genexp_filename
+      )
+    } else {
+      cat("Skipping since already downloaded\n")
+    }
   }
+    
 }
